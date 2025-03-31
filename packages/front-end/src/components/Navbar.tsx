f85b79505cc57ddfb5e14b3da5a881e7bd325b3d
Ming <757352776@qq.com>
2025-03-31 13:09:20 +0800

    feat: YL - FE - Navigation bar (UI)

--- packages/front-end/src/components/Navbar.tsx
+++ packages/front-end/src/components/Navbar.tsx
@@ -12,12 +12,23 @@ const Navbar: React.FC<NavbarProps> = () => {
           <button className="font-bold text-stone-400">Past Draw</button>
         </div>
         <div>
-          <button
+          {/* shows if not connect wallet */}
+          {/* <button
             className="bg-yuzu-green py-2 px-4 font-bold text-black yuzu-border"
             onClick={() => console.log("Connect wallet clicked")}
           >
             Connect Wallet
-          </button>
+          </button> */}
+          {/* shows if connect wallet */}
+          <span className="font-bold text-red-400">ox11BA...e185</span>
+          <svg
+            className="icon w-4 h-4 inline-block ml-2 fill-gray-300"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M534.4 553.6L262.4 284.8l-89.6 89.6 272 272 89.6 89.6 89.6-89.6 272-272-89.6-89.6z"></path>
+          </svg>
         </div>
       </div>
     </nav>

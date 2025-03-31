f52659d04d6b2cf420ad6e9a3c8b0202579406df
Ming <757352776@qq.com>
2025-03-31 12:30:16 +0800

    feat: YL - FE - Navigation bar (UI)

--- packages/front-end/src/components/Navbar.tsx
+++ packages/front-end/src/components/Navbar.tsx
@@ -4,16 +4,16 @@ interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
   return (
-    <nav className="bg-yuzu-cream rounded-lg mx-auto my-4 max-w-5xl">
-      <div className="flex justify-between items-center p-4">
+    <nav className="bg-yuzu-cream yuzu-border mx-auto mb-4 max-w-5xl">
+      <div className="flex justify-between items-center p-4 px-8">
         <div className="flex gap-4">
-          <button className="font-medium">Play</button>
-          <button className="font-medium">How It Works</button>
-          <button className="font-medium">Past Draw</button>
+          <button className="font-bold">Play</button>
+          <button className="font-bold text-stone-400">How It Works</button>
+          <button className="font-bold text-stone-400">Past Draw</button>
         </div>
         <div>
           <button
-            className="bg-yuzu-green py-2 px-4 rounded-lg font-bold text-black"
+            className="bg-yuzu-green py-2 px-4 font-bold text-black yuzu-border"
             onClick={() => console.log("Connect wallet clicked")}
           >
             Connect Wallet

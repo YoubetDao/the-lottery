4b00bddb631766148d800009afd569f5e645d80a
null0x62 <guisumail@163.com>
2025-04-12 17:08:52 +0800

    feature:connect wallet

--- packages/front-end/src/App.tsx
+++ packages/front-end/src/App.tsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TicketInfo from "./components/TicketInfo";
 import DrawHistory from "./components/DrawHistory";
 import HowToPlay from "./components/HowToPlay";
 import Footer from "./components/Footer";
+import { WagmiProvider } from "wagmi";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { eduChain, eduTestnet, projectId } from "./config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@rainbow-me/rainbowkit/styles.css";
+
+export const config = getDefaultConfig({
+  appName: "Yuzu Lottery",
+  projectId,
+  chains: [eduTestnet],
+});
 
 const App: React.FC = () => {
+  const queryClient = useMemo(() => new QueryClient({}), []);
+
   return (
-    <div className="min-h-screen bg-yuzu-dark-green pt-4">
-      <Navbar />
-      <div className="w-full flex justify-center">
-        <div className="w-full max-w-4xl px-4 md:px-8">
-          <Hero />
-          <TicketInfo />
-          <DrawHistory />
-          <HowToPlay />
-          <Footer />
-        </div>
-      </div>
-    </div>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <div className="min-h-screen bg-yuzu-dark-green pt-4">
+            <Navbar />
+            <div className="w-full flex justify-center">
+              <div className="w-full max-w-4xl px-4 md:px-8">
+                <Hero />
+                <TicketInfo />
+                <DrawHistory />
+                <HowToPlay />
+                <Footer />
+              </div>
+            </div>
+          </div>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 };
 

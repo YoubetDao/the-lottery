a5f429d55c35ad42278607d378006ca4bd3b8403
Ming <757352776@qq.com>
2025-04-14 12:05:45 +0800

    feat/buy ticket update css

--- packages/front-end/src/App.tsx
+++ packages/front-end/src/App.tsx
@@ -25,9 +25,10 @@ const App: React.FC = () => {
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
           <div className="min-h-screen bg-yuzu-dark-green pt-4 relative">
-            <Navbar />
-            <div className="clip-path-custom w-full h-1/2 bg-[#15372D] absolute top-9 z-0" />
-            <Hero />
+            <div className="bg-decorated">
+              <Navbar />
+              <Hero />
+            </div>
             <div className="max-w-4xl px-4 md:px-8 m-auto z-10 relative">
               <TicketInfo />
               <DrawHistory />

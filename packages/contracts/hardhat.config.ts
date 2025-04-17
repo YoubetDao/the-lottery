589b851636f5a2a42016a95fe950e249e27da4a9
Dylan <xjtuguozhe@foxmail.com>
2025-04-17 09:55:39 +0800

    Revert changes to hardhat.config.ts accidentally committed in fdc641ab2dfebd2fb6fc30ed56711978a7228393

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -9,21 +9,21 @@ const config: HardhatUserConfig = {
 	solidity: "0.8.28",
 
 	networks: {
-		// eduMainet: {
-		// 	chainId: 41923,
-		// 	url: "https://rpc.edu-chain.raas.gelato.cloud",
-		// 	accounts: [process.env.PRIVATE_KEY!],
-		// },
-		// eduTestnet: {
-		// 	chainId: 656476,
-		// 	url: "https://rpc.open-campus-codex.gelato.digital",
-		// 	accounts: [process.env.PRIVATE_KEY!],
-		// 	gasMultiplier: 100,
-		// 	forking: {
-		// 		enabled: true,
-		// 		url: "https://rpc.edu-chain.raas.gelato.cloud",
-		// 	},
-		// },
+		eduMainet: {
+			chainId: 41923,
+			url: "https://rpc.edu-chain.raas.gelato.cloud",
+			accounts: [process.env.PRIVATE_KEY!],
+		},
+		eduTestnet: {
+			chainId: 656476,
+			url: "https://rpc.open-campus-codex.gelato.digital",
+			accounts: [process.env.PRIVATE_KEY!],
+			gasMultiplier: 100,
+			forking: {
+				enabled: true,
+				url: "https://rpc.edu-chain.raas.gelato.cloud",
+			},
+		},
 	},
 };
 

536e1b7202e1e71be1fc7fd1f958840695ea514c
Dylan <dylanguo916@gmail.com>
2025-06-03 19:57:47 +0800

    fix(ticket-info): update label for ticket quantity display

--- packages/front-end/src/components/TicketInfo.tsx
+++ packages/front-end/src/components/TicketInfo.tsx
@@ -202,7 +202,7 @@ const TicketInfo: React.FC<TicketInfoProps> = ({
               <span className={valueStyle}>{contractInfo.nextDraw}</span>
             </div>
             <div className="flex justify-between">
-              <span className={labelStyle}>Your Ticket</span>
+              <span className={labelStyle}>Number of Tickets</span>
               <span className={valueStyle}>{contractInfo.userTickets}</span>
             </div>
           </div>
